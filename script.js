7df461a9708ff3137116e7375846a04a2740f46a
vasu1436655 <149587311+vasu1436655@users.noreply.github.com>
2023-11-02 10:40:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// document.getElementById("btn1")
+// .addEventListener("click",change1)
+
+// document.getElementById("btn2")
+// .addEventListener("click",change2)
+// function change1(){
+//     document.getElementById("name").innerText="vasu"
+//     document.getElementById("name").style.backgroundColor="yellow"
+//     document.getElementById("name").style.fontSize="80px"
+// }
+// function change2(){
+//     document.getElementById("name").innerText="vasu"
+//     document.getElementById("name").style.backgroundColor="blue"
+//     document.getElementById("name").style.fontSize="80px"
+// }
+
+
+
+// document.getElementById("btn").addEventListener("click",()=>{
+
+//     alert("i am a diologue box")
+// })
+// function change1(){
+//     document.getElementById("name").style.display="flex"
+// }
+
+// function change2(){
+//     document.getElementById("name").style.display="none"
+// }
+
+
+// function myfun(){
+//     const a=4
+//     const b=5
+//     const sum=a+b
+//     let pro=a*b
+//     document.getElementById("va").innerHTML=pro
+// }
+
+// const a="city"
+// let a="mumbai"
+// document.write(a)
